904665b122f5eebd249676f70b5506f59e2bce23
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-16 20:01:55 -0500

    10.1

---
+++ freeCodeCamp/test/10.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getDirectoryContents } = require('./utils');
+
+describe('tests', () => {
+  let projectDirectory;
+
+  before(async () => {
+    projectDirectory = await getDirectoryContents('..');
+  });
+
+  it('should pass', async () => {
+    assert(projectDirectory.indexOf('index.html') >= 0 && true);
+  });
+});
